ef2673dfe59804ec4d36a8eba1f3e8e9e7283358
cerihughes <cerihughes@116f89e2-2299-cd4e-ba00-56b364b9d20f>
2013-07-28 13:34:04 +0000

    Tidied up fading image views.
    git-svn-id: file:///Users/cerihughes/Dropbox/svn/trunk@67 116f89e2-2299-cd4e-ba00-56b364b9d20f

--- ios/Music Geek Monthly/Music Geek Monthly/MGMPulsatingAlbumView.m
+++ ios/Music Geek Monthly/Music Geek Monthly/MGMPulsatingAlbumView.m
@@ -14,8 +14,7 @@
 
 @interface MGMPulsatingAlbumView ()
 
-@property (strong) UIImageView* imageView1;
-@property (strong) UIImageView* imageView2;
+@property (strong) UIImageView* imageView;
 
 @end
 
@@ -25,51 +24,28 @@
 {
     CGSize parentSize = self.frame.size;
     CGRect frame = CGRectMake(0, 0, parentSize.width, parentSize.height);
-    self.imageView1 = [[UIImageView alloc] initWithFrame:frame];
-    self.imageView2 = [[UIImageView alloc] initWithFrame:frame];
-
-    self.imageView1.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-    self.imageView2.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-
-    self.imageView1.alpha = ALPHA_ON;
-    self.imageView2.alpha = ALPHA_OFF;
-
-    [self addSubview:self.imageView1];
-    [self addSubview:self.imageView2];
+    self.imageView = [[UIImageView alloc] initWithFrame:frame];
+    self.imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    self.imageView.alpha = ALPHA_ON;
+    [self addSubview:self.imageView];
 }
 
 - (void) renderImage:(UIImage*)image afterDelay:(NSTimeInterval)delay
 {
-    UIImageView* from;
-    UIImageView* to;
-    if (self.imageView1.alpha == ALPHA_OFF)
-    {
-        from = self.imageView2;
-        to = self.imageView1;
-    }
-    else
-    {
-        from = self.imageView1;
-        to = self.imageView2;
-    }
-
-    [MGMPulsatingAlbumView renderImage:image toImageView:to andClearImageView:from afterDelay:delay];
-}
-
-+ (void) renderImage:(UIImage*)image toImageView:(UIImageView*)newImageView andClearImageView:(UIImageView*)oldImageView afterDelay:(NSUInteger)delay
-{
-    newImageView.image = image;
     [UIView animateWithDuration:ANIMATION_TIME delay:delay options:UIViewAnimationOptionCurveEaseIn animations:^
     {
-        oldImageView.alpha = ALPHA_OFF;
+        self.imageView.alpha = ALPHA_OFF;
     }
     completion:^(BOOL finished)
     {
-        [UIView animateWithDuration:ANIMATION_TIME animations:^
+        self.imageView.image = image;
+        if (image)
         {
-            oldImageView.image = nil;
-            newImageView.alpha = ALPHA_ON;
-        }];
+            [UIView animateWithDuration:ANIMATION_TIME animations:^
+            {
+                self.imageView.alpha = ALPHA_ON;
+            }];
+        }
     }];
 }
 
