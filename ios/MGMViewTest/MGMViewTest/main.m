52f1c261cb64c742cac952e99eb35a2f1da43da8
cerihughes <cerihughes@116f89e2-2299-cd4e-ba00-56b364b9d20f>
2013-09-06 12:46:35 +0000

    Added view test project.
    git-svn-id: file:///Users/cerihughes/Dropbox/svn/trunk@124 116f89e2-2299-cd4e-ba00-56b364b9d20f

---
+++ ios/MGMViewTest/MGMViewTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MGMViewTest
+//
+//  Created by Ceri Hughes on 06/09/2013.
+//  Copyright (c) 2013 alicecallsbob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
