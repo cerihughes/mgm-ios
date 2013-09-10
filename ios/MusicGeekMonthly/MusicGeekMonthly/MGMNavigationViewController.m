9453b7c5ed40cbb3466736b9330ce84209ad4009
cerihughes <cerihughes@116f89e2-2299-cd4e-ba00-56b364b9d20f>
2013-09-10 19:08:47 +0000

    Added better icons.
    git-svn-id: file:///Users/cerihughes/Dropbox/svn/trunk@141 116f89e2-2299-cd4e-ba00-56b364b9d20f

--- ios/MusicGeekMonthly/MusicGeekMonthly/MGMNavigationViewController.m
+++ ios/MusicGeekMonthly/MusicGeekMonthly/MGMNavigationViewController.m
@@ -37,25 +37,22 @@
     homeViewController.ui = self.ui;
     homeViewController.title = @"Home";
     homeViewController.albumSelectionDelegate = self.ui;
+    homeViewController.tabBarItem.title = @"Home";
+    homeViewController.tabBarItem.image = [UIImage imageNamed:@"home.png"];
 
     MGMWeeklyChartViewController* weeklyChartViewController = [[MGMWeeklyChartViewController alloc] init];
     weeklyChartViewController.ui = self.ui;
     weeklyChartViewController.title = @"Weekly Charts";
     weeklyChartViewController.albumSelectionDelegate = self.ui;
+    weeklyChartViewController.tabBarItem.title = @"Weekly Charts";
+    weeklyChartViewController.tabBarItem.image = [UIImage imageNamed:@"chart_line.png"];
 
     MGMEventsViewController* eventsViewController = [[MGMEventsViewController alloc] init];
     eventsViewController.ui = self.ui;
     eventsViewController.title = @"Previous Events";
     eventsViewController.albumSelectionDelegate = self.ui;
-
-    UITabBarItem* homeTabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemFeatured tag:0];
-    homeTabBarItem.title = @"Home";
-    UITabBarItem* weeklyChartTabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemMostViewed tag:1];
-    UITabBarItem* eventsTabBarItem = [[UITabBarItem alloc] initWithTabBarSystemItem:UITabBarSystemItemRecents tag:2];
-    
-    homeViewController.tabBarItem = homeTabBarItem;
-    weeklyChartViewController.tabBarItem = weeklyChartTabBarItem;
-    eventsViewController.tabBarItem = eventsTabBarItem;
+    eventsViewController.tabBarItem.title = @"Previous Events";
+    eventsViewController.tabBarItem.image = [UIImage imageNamed:@"calendar.png"];
 
     [self setViewControllers: @[homeViewController, weeklyChartViewController, eventsViewController]];
 }
