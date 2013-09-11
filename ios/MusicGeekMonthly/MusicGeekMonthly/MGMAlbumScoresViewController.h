b22086282dd7f136cef832026c53c62ec083b3d5
cerihughes <cerihughes@116f89e2-2299-cd4e-ba00-56b364b9d20f>
2013-09-11 06:35:46 +0000

    Addedm the scores controller - compiling but not fully implemented.
    git-svn-id: file:///Users/cerihughes/Dropbox/svn/trunk@143 116f89e2-2299-cd4e-ba00-56b364b9d20f

---
+++ ios/MusicGeekMonthly/MusicGeekMonthly/MGMAlbumScoresViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGMAlbumScoresViewController.h
+//  MusicGeekMonthly
+//
+//  Created by Ceri Hughes on 10/09/2013.
+//  Copyright (c) 2013 Ceri Hughes. All rights reserved.
+//
+
+#import "MGMAlbumViewController.h"
+
+@interface MGMAlbumScoresViewController : MGMAlbumViewController
+
+@end
---
+++ ios/MusicGeekMonthly/MusicGeekMonthly/MGMAlbumScoresViewController.m
@@ -0,0 +1,178 @@
+//
+//  MGMAlbumScoresViewController.m
+//  MusicGeekMonthly
+//
+//  Created by Ceri Hughes on 10/09/2013.
+//  Copyright (c) 2013 Ceri Hughes. All rights reserved.
+//
+
+#import "MGMAlbumScoresViewController.h"
+
+#import "MGMCoreDataTableViewDataSource.h"
+#import "MGMGridManager.h"
+#import "MGMImageHelper.h"
+#import "MGMWeeklyChartAlbumsView.h"
+
+@interface MGMAlbumScoresViewController () <MGMWeeklyChartAlbumsViewDelegate, UITableViewDelegate>
+
+@property (strong) IBOutlet MGMWeeklyChartAlbumsView* albumsView;
+
+@property (strong) NSArray* albums;
+
+@end
+
+@implementation MGMAlbumScoresViewController
+
+- (id) init
+{
+    if (self = [super initWithNibName:nil bundle:nil])
+    {
+    }
+    return self;
+}
+
+- (IBAction) segmentSwitched:(id)sender
+{
+    
+}
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+
+    self.albumsView.delegate = self;
+
+    BOOL iPad = self.view.frame.size.width > 320;
+    NSUInteger albumCount = 25;
+    NSUInteger rowCount = iPad ? 4 : 2;
+    NSUInteger columnCount = (albumCount + 3) / rowCount;
+    CGFloat albumSize = self.albumsView.frame.size.width / rowCount;
+    NSArray* gridData = [MGMGridManager rectsForRows:rowCount columns:columnCount size:albumSize count:albumCount];
+
+    for (NSUInteger i = 0; i < albumCount; i++)
+    {
+        NSValue* value = [gridData objectAtIndex:i];
+        CGRect frame = [value CGRectValue];
+        [self.albumsView setupAlbumFrame:frame forRank:i + 1];
+    }
+}
+
+- (void) loadAlbumsForPeriod:(MGMTimePeriod*)timePeriod
+{
+    dispatch_async(dispatch_get_main_queue(), ^
+    {
+        [self.albumsView setActivityInProgressForAllRanks:YES];
+    });
+
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^
+    {
+        [self.core.daoFactory.lastFmDao fetchWeeklyChartForStartDate:timePeriod.startDate endDate:timePeriod.endDate completion:^(MGMWeeklyChart* weeklyChart, NSError* fetchError)
+        {
+            if (fetchError && weeklyChart)
+            {
+                [self logError:fetchError];
+            }
+
+            if (weeklyChart)
+            {
+//                self.weeklyChartMoid = weeklyChart.objectID;
+                [self reloadData];
+            }
+            else
+            {
+                [self showError:fetchError];
+            }
+        }];
+    });
+}
+
+- (void) reloadData
+{
+    NSUInteger position = 1;
+    for (MGMAlbum* album in self.albums)
+    {
+        if ([album searchedServiceType:MGMAlbumServiceTypeLastFm] == NO)
+        {
+            [self.core.daoFactory.lastFmDao updateAlbumInfo:album completion:^(MGMAlbum* updatedAlbum, NSError* fetchError)
+            {
+                if (fetchError && updatedAlbum)
+                {
+                    [self logError:fetchError];
+                }
+
+                if (updatedAlbum)
+                {
+                    [self renderAlbum:album atPostion:position];
+                }
+                else
+                {
+                    [self showError:fetchError];
+                }
+            }];
+        }
+        else
+        {
+            [self renderAlbum:album atPostion:position];
+        }
+        position++;
+    }
+}
+
+- (void) renderAlbum:(MGMAlbum*)album atPostion:(NSUInteger)position
+{
+    NSString* albumArtUri = [album bestAlbumImageUrl];
+    if (albumArtUri)
+    {
+        [MGMImageHelper asyncImageFromUrl:albumArtUri completion:^(UIImage* image, NSError* error)
+        {
+            if (error)
+            {
+                [self logError:error];
+            }
+
+            if (image == nil)
+            {
+                image = [self defaultImageForRank:position];
+            }
+
+            dispatch_async(dispatch_get_main_queue(), ^
+            {
+                [self.albumsView setActivityInProgress:NO forRank:position];
+                [self.albumsView setAlbumImage:image artistName:album.artistName albumName:album.albumName rank:position listeners:0 score:[album.score floatValue]];
+            });
+        }];
+    }
+    else
+    {
+        UIImage* image = [self defaultImageForRank:position];
+        dispatch_async(dispatch_get_main_queue(), ^
+        {
+            [self.albumsView setActivityInProgress:NO forRank:position];
+            [self.albumsView setAlbumImage:image artistName:album.artistName albumName:album.albumName rank:position listeners:0 score:[album.score floatValue]];
+        });
+    }
+}
+
+- (UIImage*) defaultImageForRank:(NSUInteger)rank
+{
+    NSUInteger albumType = (rank % 3) + 1;
+    NSString* imageName = [NSString stringWithFormat:@"album%d.png", albumType];
+    return [UIImage imageNamed:imageName];
+}
+
+#pragma mark -
+#pragma mark MGMWeeklyChartAlbumsViewDelegate
+
+- (void) albumPressedWithRank:(NSUInteger)rank
+{
+    MGMAlbum* album = [self.albums objectAtIndex:rank - 1];
+    [self.albumSelectionDelegate albumSelected:album];
+}
+
+- (void) detailPressedWithRank:(NSUInteger)rank
+{
+    MGMAlbum* album = [self.albums objectAtIndex:rank - 1];
+    [self.albumSelectionDelegate detailSelected:album sender:self];
+}
+
+@end
