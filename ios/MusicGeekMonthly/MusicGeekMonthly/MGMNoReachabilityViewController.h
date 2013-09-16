81572be947bd0b5f4e73a0275c9c69eefffe74f2
cerihughes <cerihughes@116f89e2-2299-cd4e-ba00-56b364b9d20f>
2013-09-16 18:24:05 +0000

    Added template controller to render when no reachability hampers the ability to display anything.
    git-svn-id: file:///Users/cerihughes/Dropbox/svn/trunk@152 116f89e2-2299-cd4e-ba00-56b364b9d20f

---
+++ ios/MusicGeekMonthly/MusicGeekMonthly/MGMNoReachabilityViewController.h
@@ -0,0 +1,13 @@
+//
+//  MGMNoReachabilityViewController.h
+//  MusicGeekMonthly
+//
+//  Created by Ceri Hughes on 16/09/2013.
+//  Copyright (c) 2013 Ceri Hughes. All rights reserved.
+//
+
+#import "MGMViewController.h"
+
+@interface MGMNoReachabilityViewController : MGMViewController
+
+@end
---
+++ ios/MusicGeekMonthly/MusicGeekMonthly/MGMNoReachabilityViewController.m
@@ -0,0 +1,25 @@
+//
+//  MGMNoReachabilityViewController.m
+//  MusicGeekMonthly
+//
+//  Created by Ceri Hughes on 16/09/2013.
+//  Copyright (c) 2013 Ceri Hughes. All rights reserved.
+//
+
+#import "MGMNoReachabilityViewController.h"
+
+@interface MGMNoReachabilityViewController ()
+
+@end
+
+@implementation MGMNoReachabilityViewController
+
+- (id)init
+{
+    if (self = [super initWithNibName:nil bundle:nil])
+    {
+    }
+    return self;
+}
+
+@end
