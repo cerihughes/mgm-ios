931b236b0c1252d69c3e16f14bd4f2228a24295c
cerihughes <cerihughes@116f89e2-2299-cd4e-ba00-56b364b9d20f>
2014-03-16 11:03:14 +0000

    More dao operation unit tests.
    git-svn-id: file:///Users/cerihughes/Dropbox/svn/trunk@314 116f89e2-2299-cd4e-ba00-56b364b9d20f

--- ios/MusicGeekMonthly/MusicGeekMonthlyTests/MGMDaoOperationTestCase.m
+++ ios/MusicGeekMonthly/MusicGeekMonthlyTests/MGMDaoOperationTestCase.m
@@ -15,6 +15,17 @@ static id _mockLocalDataSource;
 static id _mockRemoteDataSource;
 static NSUInteger _mockDaysBetweenRemoteFetch;
 
+@interface MGMMockNextUrlAccess : NSObject
+
+@property (strong) NSDate* date;
+@property (strong) NSString* checksum;
+
+@end
+
+@implementation MGMMockNextUrlAccess
+
+@end
+
 @interface MGMDaoOperationForTesting : MGMDaoOperation
 
 @end
@@ -86,50 +97,154 @@ static NSUInteger _mockDaysBetweenRemoteFetch;
     return [[NSCalendar currentCalendar] dateByAddingComponents:components toDate:[NSDate date] options:0];
 }
 
-- (void) testInitialFetch
+- (void) expect_MGMCoreDataAccess_fetchNextUrlAccessWithIdentifier:(id)key andReturnMoid:(NSManagedObjectID*)moid andReturnError:(NSError*)error
 {
-    NSManagedObjectID* mockNextUrlAccessMoid = [OCMockObject mockForClass:[NSManagedObjectID class]];
-
     [[[self.mockCoreDataAccess expect] andDo:^(NSInvocation* invocation) {
         __unsafe_unretained CORE_DATA_FETCH_COMPLETION completion;
         [invocation getArgument:&completion atIndex:3];
-        completion(mockNextUrlAccessMoid, nil);
-    }] fetchNextUrlAccessWithIdentifier:@"KEY" completion:OCMOCK_ANY];
+        completion(moid, error);
+    }] fetchNextUrlAccessWithIdentifier:key completion:OCMOCK_ANY];
+}
 
-    [[[self.mockCoreDataAccess expect] andReturn:nil] mainThreadVersion:mockNextUrlAccessMoid];
-
-    MGMRemoteData* remoteData = [[MGMRemoteData alloc] init];
-    remoteData.data = @"SomeRemoteData";
-    remoteData.checksum = @"SomeRemoteDataChecksum";
-
+- (void) expect_MGMRemoteDataSource_fetchRemoteData:(id)key andReturn:(MGMRemoteData*)remoteData
+{
     [[[_mockRemoteDataSource expect] andDo:^(NSInvocation* invocation) {
         __unsafe_unretained REMOTE_DATA_FETCH_COMPLETION completion;
         [invocation getArgument:&completion atIndex:3];
         completion(remoteData);
-    }] fetchRemoteData:@"KEY" completion:OCMOCK_ANY];
+    }] fetchRemoteData:key completion:OCMOCK_ANY];
+}
 
+- (void) expect_MGMLocalDataSource_persistRemoteData:(MGMRemoteData*)remoteData key:(id)key andReturn:(NSError*)error
+{
     [[[_mockLocalDataSource expect] andDo:^(NSInvocation* invocation) {
         __unsafe_unretained LOCAL_DATA_PERSIST_COMPLETION completion;
         [invocation getArgument:&completion atIndex:4];
-        completion(nil);
-    }] persistRemoteData:remoteData key:@"KEY" completion:OCMOCK_ANY];
+        completion(error);
+    }] persistRemoteData:remoteData key:key completion:OCMOCK_ANY];
+}
 
+- (void) expect_MGMCoreDataAccess_persistNextUrlAccess:(id)key
+{
     [[[self.mockCoreDataAccess expect] andDo:^(NSInvocation* invocation) {
         // TODO: Test date difference?
 
         __unsafe_unretained LOCAL_DATA_PERSIST_COMPLETION completion;
         [invocation getArgument:&completion atIndex:4];
         completion(nil);
-    }] persistNextUrlAccess:@"KEY" date:OCMOCK_ANY completion:OCMOCK_ANY];
+    }] persistNextUrlAccess:key date:OCMOCK_ANY completion:OCMOCK_ANY];
+}
 
-    MGMLocalData* localData = [[MGMLocalData alloc] init];
-    localData.data = @"SomeLocalData";
-
+- (void) expect_MGMLocalDataSource_fetchLocalData:(id)key andReturn:(MGMLocalData*)localData
+{
     [[[_mockLocalDataSource expect] andDo:^(NSInvocation* invocation) {
         __unsafe_unretained LOCAL_DATA_FETCH_COMPLETION completion;
         [invocation getArgument:&completion atIndex:3];
         completion(localData);
-    }] fetchLocalData:@"KEY" completion:OCMOCK_ANY];
+    }] fetchLocalData:key completion:OCMOCK_ANY];
+}
+
+- (void) testInitialFetch
+{
+    NSManagedObjectID* mockNextUrlAccessMoid = [OCMockObject mockForClass:[NSManagedObjectID class]];
+    [self expect_MGMCoreDataAccess_fetchNextUrlAccessWithIdentifier:@"KEY" andReturnMoid:mockNextUrlAccessMoid andReturnError:nil];
+    [[[self.mockCoreDataAccess expect] andReturn:nil] mainThreadVersion:mockNextUrlAccessMoid];
+
+    MGMRemoteData* remoteData = [[MGMRemoteData alloc] init];
+    remoteData.data = @"SomeRemoteData";
+    remoteData.checksum = @"SomeRemoteDataChecksum";
+
+    [self expect_MGMRemoteDataSource_fetchRemoteData:@"KEY" andReturn:remoteData];
+    [self expect_MGMLocalDataSource_persistRemoteData:remoteData key:@"KEY" andReturn:nil];
+    [self expect_MGMCoreDataAccess_persistNextUrlAccess:@"KEY"];
+
+    MGMLocalData* localData = [[MGMLocalData alloc] init];
+    localData.data = @"SomeLocalData";
+
+    [self expect_MGMLocalDataSource_fetchLocalData:@"KEY" andReturn:localData];
+
+    // Let's go...
+    [self.cut fetchData:@"KEY" completion:^(MGMDaoData* daoData) {
+        XCTAssertNil(daoData.error, @"Expecting no error.");
+        XCTAssertEqualObjects(@"SomeLocalData", daoData.data, @"Expecting equlity.");
+        XCTAssertTrue(daoData.isNew, @"Expecting new data");
+    }];
+}
+
+- (void) testSubsequentFetchWithin24Hours
+{
+    NSManagedObjectID* mockNextUrlAccessMoid = [OCMockObject mockForClass:[NSManagedObjectID class]];
+    [self expect_MGMCoreDataAccess_fetchNextUrlAccessWithIdentifier:@"KEY" andReturnMoid:mockNextUrlAccessMoid andReturnError:nil];
+
+    MGMMockNextUrlAccess* nextUrlAccess = [[MGMMockNextUrlAccess alloc] init];
+    nextUrlAccess.date = [self daysAfterNow:1];
+    [[[self.mockCoreDataAccess expect] andReturn:nextUrlAccess] mainThreadVersion:mockNextUrlAccessMoid];
+
+    MGMLocalData* localData = [[MGMLocalData alloc] init];
+    localData.data = @"SomeLocalData";
+
+    [self expect_MGMLocalDataSource_fetchLocalData:@"KEY" andReturn:localData];
+
+    // Let's go...
+    [self.cut fetchData:@"KEY" completion:^(MGMDaoData* daoData) {
+        XCTAssertNil(daoData.error, @"Expecting no error.");
+        XCTAssertEqualObjects(@"SomeLocalData", daoData.data, @"Expecting equlity.");
+        XCTAssertFalse(daoData.isNew, @"Expecting old data");
+    }];
+}
+
+- (void) testSubsequentFetchAfter24HoursUnchangedData
+{
+    NSManagedObjectID* mockNextUrlAccessMoid = [OCMockObject mockForClass:[NSManagedObjectID class]];
+    [self expect_MGMCoreDataAccess_fetchNextUrlAccessWithIdentifier:@"KEY" andReturnMoid:mockNextUrlAccessMoid andReturnError:nil];
+
+    MGMMockNextUrlAccess* nextUrlAccess = [[MGMMockNextUrlAccess alloc] init];
+    nextUrlAccess.date = [self daysAfterNow:0];
+    nextUrlAccess.checksum = @"SomeRemoteDataChecksum";
+    [[[self.mockCoreDataAccess expect] andReturn:nextUrlAccess] mainThreadVersion:mockNextUrlAccessMoid];
+
+    MGMRemoteData* remoteData = [[MGMRemoteData alloc] init];
+    remoteData.data = @"SomeRemoteData";
+    remoteData.checksum = @"SomeRemoteDataChecksum";
+
+    [self expect_MGMRemoteDataSource_fetchRemoteData:@"KEY" andReturn:remoteData];
+    [self expect_MGMCoreDataAccess_persistNextUrlAccess:@"KEY"];
+
+    MGMLocalData* localData = [[MGMLocalData alloc] init];
+    localData.data = @"SomeLocalData";
+
+    [self expect_MGMLocalDataSource_fetchLocalData:@"KEY" andReturn:localData];
+
+    // Let's go...
+    [self.cut fetchData:@"KEY" completion:^(MGMDaoData* daoData) {
+        XCTAssertNil(daoData.error, @"Expecting no error.");
+        XCTAssertEqualObjects(@"SomeLocalData", daoData.data, @"Expecting equlity.");
+        XCTAssertFalse(daoData.isNew, @"Expecting old data");
+    }];
+}
+
+- (void) testSubsequentFetchAfter24HoursChangedData
+{
+    NSManagedObjectID* mockNextUrlAccessMoid = [OCMockObject mockForClass:[NSManagedObjectID class]];
+    [self expect_MGMCoreDataAccess_fetchNextUrlAccessWithIdentifier:@"KEY" andReturnMoid:mockNextUrlAccessMoid andReturnError:nil];
+
+    MGMMockNextUrlAccess* nextUrlAccess = [[MGMMockNextUrlAccess alloc] init];
+    nextUrlAccess.date = [self daysAfterNow:0];
+    nextUrlAccess.checksum = @"SomeRemoteDataChecksum";
+    [[[self.mockCoreDataAccess expect] andReturn:nextUrlAccess] mainThreadVersion:mockNextUrlAccessMoid];
+
+    MGMRemoteData* remoteData = [[MGMRemoteData alloc] init];
+    remoteData.data = @"SomeRemoteData";
+    remoteData.checksum = @"AnotherRemoteDataChecksum";
+
+    [self expect_MGMRemoteDataSource_fetchRemoteData:@"KEY" andReturn:remoteData];
+    [self expect_MGMLocalDataSource_persistRemoteData:remoteData key:@"KEY" andReturn:nil];
+    [self expect_MGMCoreDataAccess_persistNextUrlAccess:@"KEY"];
+
+    MGMLocalData* localData = [[MGMLocalData alloc] init];
+    localData.data = @"SomeLocalData";
+
+    [self expect_MGMLocalDataSource_fetchLocalData:@"KEY" andReturn:localData];
 
     // Let's go...
     [self.cut fetchData:@"KEY" completion:^(MGMDaoData* daoData) {
