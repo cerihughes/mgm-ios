127d2387cffeef43931858d218522fedef90f511
ceri <ceri@spotify.com>
2016-03-06 18:06:59 +0100

    Convert album scores view to adaptive layout

--- ios/MusicGeekMonthly/MusicGeekMonthly/UI/NSLayoutConstraint+MGM.h
+++ ios/MusicGeekMonthly/MusicGeekMonthly/UI/NSLayoutConstraint+MGM.h
@@ -12,14 +12,21 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-+(instancetype)constraintWithItem:(id)view1
-                        attribute:(NSLayoutAttribute)attr1
-                        relatedBy:(NSLayoutRelation)relation
-                           toItem:(nullable id)view2
-                        attribute:(NSLayoutAttribute)attr2
-                       multiplier:(CGFloat)multiplier
-                         constant:(CGFloat)c
-                         priority:(UILayoutPriority)priority;
++ (instancetype)constraintWithItem:(id)view1
+                         attribute:(NSLayoutAttribute)attr1
+                         relatedBy:(NSLayoutRelation)relation
+                            toItem:(nullable id)view2
+                         attribute:(NSLayoutAttribute)attr2
+                        multiplier:(CGFloat)multiplier
+                          constant:(CGFloat)c
+                          priority:(UILayoutPriority)priority;
+
++ (NSArray<__kindof NSLayoutConstraint *> *)constraintsWithItem:(id)view1
+                                   thatMatchCenterAndSizeOfItem:(id)view2;
+
++ (NSArray<__kindof NSLayoutConstraint *> *)constraintsWithItem:(id)view1
+                                   thatMatchCenterAndSizeOfItem:(id)view2
+                                                       priority:(UILayoutPriority)priority;
 
 NS_ASSUME_NONNULL_END
 
--- ios/MusicGeekMonthly/MusicGeekMonthly/UI/NSLayoutConstraint+MGM.m
+++ ios/MusicGeekMonthly/MusicGeekMonthly/UI/NSLayoutConstraint+MGM.m
@@ -10,18 +10,71 @@
 
 @implementation NSLayoutConstraint (MGM)
 
-+(instancetype)constraintWithItem:(id)view1
-                        attribute:(NSLayoutAttribute)attr1
-                        relatedBy:(NSLayoutRelation)relation
-                           toItem:(nullable id)view2
-                        attribute:(NSLayoutAttribute)attr2
-                       multiplier:(CGFloat)multiplier
-                         constant:(CGFloat)c
-                         priority:(UILayoutPriority)priority
++ (instancetype)constraintWithItem:(id)view1
+                         attribute:(NSLayoutAttribute)attr1
+                         relatedBy:(NSLayoutRelation)relation
+                            toItem:(nullable id)view2
+                         attribute:(NSLayoutAttribute)attr2
+                        multiplier:(CGFloat)multiplier
+                          constant:(CGFloat)c
+                          priority:(UILayoutPriority)priority
 {
     NSLayoutConstraint *constraint = [self constraintWithItem:view1 attribute:attr1 relatedBy:relation toItem:view2 attribute:attr2 multiplier:multiplier constant:c];
     constraint.priority = priority;
     return constraint;
+}
+
++ (NSArray<__kindof NSLayoutConstraint *> *)constraintsWithItem:(id)view1
+                                   thatMatchCenterAndSizeOfItem:(id)view2
+{
+    return [self constraintsWithItem:view1
+        thatMatchCenterAndSizeOfItem:view2
+                            priority:UILayoutPriorityRequired];
+}
+
++ (NSArray<__kindof NSLayoutConstraint *> *)constraintsWithItem:(id)view1
+                                   thatMatchCenterAndSizeOfItem:(id)view2
+                                                       priority:(UILayoutPriority)priority
+{
+    NSMutableArray<__kindof NSLayoutConstraint *> *constraints = [NSMutableArray array];
+
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:view1
+                                                        attribute:NSLayoutAttributeCenterX
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:view2
+                                                        attribute:NSLayoutAttributeCenterX
+                                                       multiplier:1
+                                                         constant:0
+                                                         priority:priority]];
+
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:view1
+                                                        attribute:NSLayoutAttributeCenterY
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:view2
+                                                        attribute:NSLayoutAttributeCenterY
+                                                       multiplier:1
+                                                         constant:0
+                                                         priority:priority]];
+
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:view1
+                                                        attribute:NSLayoutAttributeWidth
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:view2
+                                                        attribute:NSLayoutAttributeWidth
+                                                       multiplier:1
+                                                         constant:0
+                                                         priority:priority]];
+
+    [constraints addObject:[NSLayoutConstraint constraintWithItem:view1
+                                                        attribute:NSLayoutAttributeHeight
+                                                        relatedBy:NSLayoutRelationEqual
+                                                           toItem:view2
+                                                        attribute:NSLayoutAttributeHeight
+                                                       multiplier:1
+                                                         constant:0
+                                                         priority:priority]];
+
+    return constraints;
 }
 
 @end
