52752c6fbb7205686a7c12f4fedd6dc17277f371
Ceri Hughes <github@cerihughes.co.uk>
2018-11-07 14:59:23 +0000

    Plug it in

--- ios/MusicGeekMonthlySwift/MusicGeekMonthly/LatestEvent/LatestEventView.swift
+++ ios/MusicGeekMonthlySwift/MusicGeekMonthly/LatestEvent/LatestEventView.swift
@@ -5,7 +5,7 @@ class LatestEventView: DataLoadingView {
     let collectionView: UICollectionView
 
     override init(frame: CGRect) {
-        let layout = FullWidthCollectionViewLayout(itemHeight: 128.0, spacing: 4.0)
+        let layout = FullWidthCollectionViewLayout(defaultItemHeight: 128.0, itemHeightOverrides:[0 : 192.0], spacing: 4.0)
         collectionView = UICollectionView(frame: frame, collectionViewLayout: layout)
 
         super.init(frame: frame)
