039f9f5f6c0b838ebabe48e3565a8afcfc299d05
Ceri Hughes <github@cerihughes.co.uk>
2018-11-10 12:07:10 +0000

    Delete old versions

--- ios/MusicGeekMonthly/MusicGeekMonthly/Core/MGMGridManager.h
+++
@@ -1,9 +0,0 @@
-
-@import Foundation;
-@import CoreGraphics;
-
-@interface MGMGridManager : NSObject
-
-+ (NSArray*) rectsForRowSize:(NSUInteger)rowSize defaultRectSize:(CGFloat)defaultRectSize count:(NSUInteger)count;
-
-@end
