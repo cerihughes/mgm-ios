d174eed16d2d544d45adc8c45387a178b5848365
Ceri Hughes <github@cerihughes.co.uk>
2019-02-19 15:28:36 +0000

    Add the award colour

--- android/app/src/main/java/uk/co/cerihughes/mgm/android/ui/albumscores/AlbumAward.kt
+++ android/app/src/main/java/uk/co/cerihughes/mgm/android/ui/albumscores/AlbumAward.kt
@@ -1,13 +1,15 @@
 package uk.co.cerihughes.mgm.android.ui.albumscores
 
+import android.graphics.Color
+import android.support.annotation.ColorInt
 import android.support.annotation.DrawableRes
 import uk.co.cerihughes.mgm.android.R
 
-enum class AlbumAward(@DrawableRes val drawable: Int) {
-    GOLD(R.drawable.award_gold),
-    SILVER(R.drawable.award_silver),
-    BRONZE(R.drawable.award_plate),
-    NONE(R.drawable.award_none);
+enum class AlbumAward(@DrawableRes val drawable: Int, @ColorInt val colour: Int) {
+    GOLD(R.drawable.award_gold, Color.argb(255, 238, 187, 100)),
+    SILVER(R.drawable.award_silver, Color.argb(255, 180, 180, 185)),
+    BRONZE(R.drawable.award_plate, Color.argb(255, 217, 162, 129)),
+    NONE(R.drawable.award_none, Color.argb(255, 55, 106, 77));
 
     companion object {
         fun createAward(score: Float): AlbumAward {
@@ -22,10 +24,5 @@ enum class AlbumAward(@DrawableRes val drawable: Int) {
             }
             return NONE
         }
-    }
-
-    @DrawableRes
-    fun awardImage(): Int {
-        return drawable
     }
 }
