ee8c8c57d5fe5d51c02ea0540c0d7ce3a1d368e7
Ceri Hughes <github@cerihughes.co.uk>
2019-03-01 16:37:09 +0000

    Sort albums by score (desc), name (asc), artist (asc)

--- android/app/src/main/java/uk/co/cerihughes/mgm/android/ui/albumscores/AlbumScoresViewModel.kt
+++ android/app/src/main/java/uk/co/cerihughes/mgm/android/ui/albumscores/AlbumScoresViewModel.kt
@@ -14,9 +14,19 @@ class AlbumScoresViewModel : SpotifyAwareViewModel() {
     fun isLoaded(): Boolean = allAlbums.size > 0
 
     fun setEvents(events: List<Event>) {
-        classicAlbums = events.mapNotNull { it.classicAlbum }.filter { it.score != null }.sortedByDescending { it.score }
-        newAlbums = events.mapNotNull { it.newAlbum }.filter { it.score != null }.sortedByDescending { it.score }
-        allAlbums = (classicAlbums + newAlbums).sortedByDescending { it.score }
+        val comparator = compareByDescending<Album> { it.score }
+            .thenBy { it.name }
+            .thenBy { it.artist }
+
+        classicAlbums = events.mapNotNull { it.classicAlbum }
+            .filter { it.score != null }
+            .sortedWith(comparator)
+
+        newAlbums = events.mapNotNull { it.newAlbum }
+            .filter { it.score != null }
+            .sortedWith(comparator)
+
+        allAlbums = (classicAlbums + newAlbums).sortedWith(comparator)
         scoreViewModels = allAlbums.mapIndexed { index, it -> AlbumScoreViewModel(it, index) }
     }
 
