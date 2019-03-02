dea8c3c06585a94e35e5fbea41877febbbe7803f
Ceri Hughes <github@cerihughes.co.uk>
2019-03-02 17:18:24 +0000

    Organise imports

--- android/app/src/main/java/uk/co/cerihughes/mgm/android/dataloader/fallback/FallbackEventLoader.kt
+++ android/app/src/main/java/uk/co/cerihughes/mgm/android/dataloader/fallback/FallbackEventLoader.kt
@@ -3,8 +3,8 @@ package uk.co.cerihughes.mgm.android.dataloader.fallback
 import android.content.Context
 import uk.co.cerihughes.mgm.android.R
 import uk.co.cerihughes.mgm.android.dataloader.EventLoader
-import uk.co.cerihughes.mgm.android.repository.GsonFactory
 import uk.co.cerihughes.mgm.android.model.Event
+import uk.co.cerihughes.mgm.android.repository.GsonFactory
 
 class FallbackEventLoader(context: Context): EventLoader {
     private val fallbackData: String
