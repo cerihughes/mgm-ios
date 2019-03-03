e55b30f7e09c993e0e879d57ae2d491b1767ec9d
Ceri Hughes <github@cerihughes.co.uk>
2019-03-03 18:12:05 +0000

    Maintain state on rotation

--- android/app/src/main/java/uk/co/cerihughes/mgm/android/ui/MainActivity.kt
+++ android/app/src/main/java/uk/co/cerihughes/mgm/android/ui/MainActivity.kt
@@ -8,11 +8,14 @@ import android.view.MenuItem
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.viewmodel.ext.android.viewModel
 import uk.co.cerihughes.mgm.android.R
 import uk.co.cerihughes.mgm.android.ui.albumscores.AlbumScoresFragment
 import uk.co.cerihughes.mgm.android.ui.latestevent.LatestEventFragment
 
 class MainActivity : AppCompatActivity() {
+
+    val viewModel: MainViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,23 +23,29 @@ class MainActivity : AppCompatActivity() {
         Fabric.with(this, Crashlytics())
 
         setContentView(R.layout.activity_main)
-        loadFragment(LatestEventFragment())
+        loadFragment(viewModel.selectedItemId)
 
         navigation.setOnNavigationItemSelectedListener(object: BottomNavigationView.OnNavigationItemSelectedListener {
             override fun onNavigationItemSelected(item: MenuItem): Boolean {
-                when (item.itemId) {
-                    R.id.navigation_latest_event -> {
-                        loadFragment(LatestEventFragment())
-                        return true
-                    }
-                    R.id.navigation_album_scores -> {
-                        loadFragment(AlbumScoresFragment())
-                        return true
-                    }
-                }
-                return false
+                return loadFragment(item.itemId)
             }
         })
+    }
+
+    private fun loadFragment(itemId: Int): Boolean {
+        viewModel.selectedItemId = itemId
+
+        when (itemId) {
+            R.id.navigation_latest_event -> {
+                loadFragment(LatestEventFragment())
+                return true
+            }
+            R.id.navigation_album_scores -> {
+                loadFragment(AlbumScoresFragment())
+                return true
+            }
+        }
+        return false
     }
 
     private fun loadFragment(fragment: Fragment) {
