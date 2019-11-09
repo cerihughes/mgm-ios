21b60805cfc6968e2874f99c19071f1f5c6d1d23
Ceri Hughes <github@cerihughes.co.uk>
2019-11-09 15:25:50 +0000

    Remvoe secrets

--- BuildPhases/run-fabric.sh
+++ BuildPhases/run-fabric.sh
@@ -1,4 +1,1 @@
-apiKey=$(sed -n '1p' < Secrets.txt)
-secretKey=$(sed -n '2p' < Secrets.txt)
-
-"${PROJECT_DIR}/Frameworks/Fabric.framework/run" $apiKey $secretKey
+"${PROJECT_DIR}/Frameworks/Fabric.framework/run"
