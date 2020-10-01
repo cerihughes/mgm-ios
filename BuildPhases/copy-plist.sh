7ca0d2e7e04fe0a0eab0ada9064e8886ba1df243
Ceri Hughes <github@cerihughes.co.uk>
2020-10-01 15:53:46 +0100

    Use copy-plist from tools

--- BuildPhases/copy-plist.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-cp $PROJECT_DIR/Source/Info.plist $PROJECT_DIR/Source/Secret.plist
-sed "s/_BUILD_NUMBER_/`git rev-list HEAD --count`/" $PROJECT_DIR/Source/Info.plist > $PROJECT_DIR/Source/Secret.plist
